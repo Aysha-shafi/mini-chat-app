2eaea64ecfaa681360801362174c0463ed4b2ac1
Aysha Shafi <ayshashafi@Ayshas-MacBook-Air.local>
2025-10-31 17:00:58 +0530

    backend url added

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,4 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000"); 
+
+const socket = io("https://mini-chat-app-4.onrender.com", {
+  transports: ["websocket"],
+});
+
 export default socket;
